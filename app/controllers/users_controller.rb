7643b7e40fc1d5baf0ff76fd93f2c1a0168ea833
Josphat-Kip <josphatkiplimo247@gmail.com>
2023-04-06 06:13:58 +0300

    Modified users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,16 @@ class UsersController < ApplicationController
      user = User.find(params[:id])
      render json: user
    end
+
+  #  def create
+  #   @user = User.new(user_params)
+  #   if @user.save!
+  #     session[:user_id] = @user.id
+  #     render json: @user, status: :created
+  #   else
+  #     render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
  
    def create
      user = User.create!(user_params)
